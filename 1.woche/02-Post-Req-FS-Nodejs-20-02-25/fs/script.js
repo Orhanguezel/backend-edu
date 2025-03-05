c6e2e6d06ba71d3b6ac442478520d9a540ea0a55
Orhan Güzel <orhan.guezel@dci-student.org>
2025-03-06 00:36:51 +0100

    Gereksiz dosyalar takipten çıkarıldı

--- 1.woche/02-Post-Req-FS-Nodejs-20-02-25/fs/script.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-fs.writeFileSync('test.txt', 'Hello World');
-//fs.writeFileSync('test.js', 'Hello World', 'utf-8');
-
-console.log(fs.readFileSync('test.txt', 'utf-8'));
--- 2.woche/05-Express-Router-27-02-25/live-coding-express-router/middleware/requestLogger.js
+++
@@ -1,4 +0,0 @@
-export default function requestLogger(req, res, next) {
-  console.log(`REQ : ${req.method} | ${req.url}`);
-  next();
-}
